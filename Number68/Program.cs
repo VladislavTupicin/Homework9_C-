9399e94638b2600891e9b9117c30450fd5d5b0c1
«Vlad» <>
2022-09-27 17:05:10 +0300

    Homework9

---
+++ Number68/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+int nod(int n, int m){
+    if(m==0)
+        return n;
+    if(n>m)  
+        return nod(m,n%m);
+    else
+        return nod(n,m%n);
+}
+
+int main (){ 
+    std::cout << nod(12,8);
+    system("PAUSE");
+    return 0;
+}
+*/
